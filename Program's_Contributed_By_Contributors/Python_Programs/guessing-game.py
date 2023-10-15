f8e3c1cbcf41caad916f4803825cedd478f43ab8
Perivitta Rajendran <46223444+PR-Peri@users.noreply.github.com>
2023-10-15 20:53:41 +0800

    Create guessing-game.py

---
+++ Program's_Contributed_By_Contributors/Python_Programs/guessing-game.py
@@ -0,0 +1,28 @@
+import random
+
+def guess_the_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100. Can you guess it?")
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
+        attempts += 1
+
+        if guess < secret_number:
+            print("Higher! Try again.")
+        elif guess > secret_number:
+            print("Lower! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts!")
+            break
+
+if __name__ == "__main__":
+    guess_the_number()
